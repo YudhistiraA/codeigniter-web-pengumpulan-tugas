6ac7cb24ea5f817fc2e678d9909b0222ecb38c6b
yudhistira arya <yudhistira_arya_27rpl@student.smktelkom-mlg.sch.id>
2020-11-29 18:46:18 +0700

    Delete nomer1.js

--- modul1/nomer1.js
+++
@@ -1,8 +0,0 @@
-let lebar = 20.5 ;
-let panjang = 30 ;
-let harga = 1500000;
-let ppn = 0.15;
-let luas = lebar * panjang ;
-let hasil = luas * harga ;
-let total = hasil * ppn ;
-console.log('total =' + ' ' + total);
